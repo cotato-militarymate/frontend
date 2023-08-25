d40fefbe8a0ee2c34ee55437dbbd494276c100f0
lsy20140 <lsy20140@naver.com>
2023-08-25 23:01:59 +0900

    feat: Input 컴포넌트 구현

---
+++ src/components/common/Input.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Input({label, name, placeholder, value, handleOnChange}) {
+  const InputStyle = {
+    padding: '0.6rem 0.8rem',
+    fontSize:'1rem',
+    color: 'black',
+    border: '1px solid #9FA6B2',
+    borderRadius: '0.5rem',
+    boxSizing: 'border-box',
+  }
+  
+  return (
+    <InputContainer>
+      <label>{label}</label>
+      <input 
+        type='text'
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        style={InputStyle}
+        onChange={handleOnChange}
+      />
+    </InputContainer>
+  )
+}
+
+const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  label{
+    font-size: 1.125rem;
+    font-weight: 500;
+
+  }
+`
